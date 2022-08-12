a1f62b6a0aa53da6cd9a52e149cd37ec91629eba
accopm <87272875+orangeaccomp@users.noreply.github.com>
2022-08-12 17:58:28 +0200

    add nicer file path

--- src/main/java/com/accomp/app/App.java
+++ src/main/java/com/accomp/app/App.java
@@ -8,11 +8,12 @@ import javax.imageio.ImageIO;
 public class App {
     public static void main(String[] args) {
         try {
-            BufferedImage baseImage = ImageIO.read(new File("./data/clearMap3.png"));
+
+            BufferedImage baseImage = ImageIO.read(new File("data" + File.separator + "clearMap3.png"));
             TileMeta meta = new TileMeta(10, 10, baseImage);
             Board board = new Board(meta, 50, 50); // TODO if width != height image is broken
             board.run();
-            board.buildImage("./generated");
+            board.buildImage("generated");
         } catch (Exception e) {
             e.printStackTrace();
         }
